915d63d20a4efef8f100ef948ec9ff35c77e77da
Saumya Modi <saumya20244@gmail.com>
2025-08-17 13:28:01 +0530

    react-created woth tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-center text-blue-500">This is React Project</h1>
+    </div>
+  )
+}
+
+export default App
